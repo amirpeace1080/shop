d1807d18e36ea4a8fae87f47f321b564c0fa5771
amirpeace1080 <amirpeace1080@gmail.com>
2020-10-31 22:56:57 +0330

    end:footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,25 +1,221 @@
 <template>
-  <div class="footer">
-    <div class="columns container">
-      <div class="column">
-        <p>Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.</p>
+  <div class="footer-page mt-5">
+    <!-- FOOTER START -->
+    <div class="footer">
+      <div class="contain">
+        <div class="col social">
+          <h1>شبکه های اجتماعی</h1>
+          <ul>
+            <li>
+              <a href="https://instagram.com/peace_1080">
+                <span
+                class="fa fa-instagram fa-2x"
+                width="32"
+                style="width: 32px; color:white"
+              />
+              </a>
+            </li>
+            <li>
+              <a href="https://telegram.me/peace1080">
+                <span
+                class="fa fa-telegram fa-2x"
+                width="32"
+                style="width: 32px; color:white"
+              />
+              </a>
+            </li>
+            <li>
+              <a href="https://telegram.me/peace1080">
+                <span
+                class="fa fa-linkedin fa-2x"
+                width="32"
+                style="width: 32px; color:white"
+              />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>پشتیبانی</h1>
+          <ul>
+            <li>تماس با ما</li>
+            <li>چت در سایت</li>
+            <li>ارسال تیکت</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>اکانت ها</h1>
+          <ul>
+            <li>درباره ما</li>
+            <li>ماموریت</li>
+            <li>سرویس ها</li>
+            <li>اجتماعی</li>
+            <li>در تماس باشید</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>منابع</h1>
+          <ul>
+            <li>ایمیل</li>
+            <li>کد تخفیف</li>
+            <li>جستجو</li>
+            <li>نقشه سایت</li>
+            <li>صفحات سایت</li>
+            <li>صفحه ایمیل</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>محصولات</h1>
+          <ul>
+            <li>درباره ما</li>
+            <li>ماموریت</li>
+            <li>سرویس ها</li>
+            <li>اجتماعی</li>
+            <li>در تماس باشید</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>شرکت</h1>
+          <ul>
+            <li>درباره ما</li>
+            <li>ماموریت</li>
+            <li>سرویس ها</li>
+            <li>اجتماعی</li>
+            <li>در تماس باشید</li>
+          </ul>
+        </div>
+        <div class="clearfix"></div>
       </div>
-    <div class="column has-text-right">
-      <p>Vuemmerce | Made with ❤</p>
     </div>
-    </div>
+    <!-- END OF FOOTER -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'VmFooter',
-}
+  name: "VmFooter"
+};
 </script>
 
 <style lang="scss" scoped>
- .footer {
-   background: #b9e2fc;
-   color: black;
- }
+//  .footer-page {
+//    background: #b9e2fc;
+//    color: black;
+//  }
+
+/* REMOVE THIS, USE YOUR OWN  */
+.footer-page {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, Sans-serif;
+  background-color: #070617;
+}
+.dummy_page {
+  height: 200px;
+  width: 100%;
+  background-color: #f0f0f0;
+  text-align: center;
+  box-sizing: border-box;
+  padding: 60px 0px;
+}
+/* STYLES SPECIFIC TO FOOTER  */
+.footer {
+  width: 100%;
+  position: relative;
+  height: auto;
+  background-color: #070617;
+}
+.footer .col {
+  width: 190px;
+  height: auto;
+  float: left;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  padding: 0px 20px 20px 20px;
+}
+.footer .col h1 {
+  margin: 0;
+  padding: 0;
+  font-family: inherit;
+  font-size: 12px;
+  line-height: 17px;
+  padding: 20px 0px 5px 0px;
+  color: rgba(255, 255, 255, 0.2);
+  font-weight: normal;
+  text-transform: uppercase;
+  letter-spacing: 0.25em;
+}
+.footer .col ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.footer .col ul li {
+  color: #999999;
+  font-size: 14px;
+  font-family: inherit;
+  font-weight: bold;
+  padding: 5px 0px 5px 0px;
+  cursor: pointer;
+  transition: 0.2s;
+  -webkit-transition: 0.2s;
+  -moz-transition: 0.2s;
+}
+.social ul li {
+  display: inline-block;
+  padding-right: 5px !important;
+}
+
+.footer .col ul li:hover {
+  color: #ffffff;
+  transition: 0.1s;
+  -webkit-transition: 0.1s;
+  -moz-transition: 0.1s;
+}
+.clearfix {
+  clear: both;
+}
+@media only screen and (min-width: 1280px) {
+  .contain {
+    width: 1200px;
+    margin: 0 auto;
+  }
+}
+@media only screen and (max-width: 1139px) {
+  .contain .social {
+    width: 1000px;
+    display: block;
+  }
+  .social h1 {
+    margin: 0px;
+  }
+}
+@media only screen and (max-width: 950px) {
+  .footer .col {
+    width: 33%;
+  }
+  .footer .col h1 {
+    font-size: 14px;
+  }
+  .footer .col ul li {
+    font-size: 13px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .footer .col {
+    width: 50%;
+  }
+  .footer .col h1 {
+    font-size: 14px;
+  }
+  .footer .col ul li {
+    font-size: 13px;
+  }
+}
+@media only screen and (max-width: 340px) {
+  .footer .col {
+    width: 100%;
+  }
+}
 </style>
